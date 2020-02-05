77bf3649ec767208c3c6249d92d6ceb819af51cc
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-05 11:02:23 +0000

    Updated Comments

--- RestSharp API Testing Tutorial/UnitTest1.cs
+++ RestSharp API Testing Tutorial/UnitTest1.cs
@@ -14,8 +14,10 @@ namespace RestSharp_API_Testing_Tutorial
         [Test]
         public void TestMethod1()
         {
+            // Establishes our client and allows it to connect to the URL
             var client = new RestClient("http://localhost:3000/");
 
+            // In Postman we can use the GET to specfify specfific data
             var request = new RestRequest("posts/{postid}", Method.GET);
             request.AddUrlSegment("postid", 1);
 
